9c8545b8a5f734494a32d5db2423f7cef7a5ad00
SeungGukYoo <dbtmd878@gmail.com>
2023-11-08 23:40:19 +0900

    feat: react-toastify 컴포넌트 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import App from 'App';
-
-import ReactDOM from 'react-dom/client';
-
 import AuthContextProvider from 'context/authContext';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Router>
     <AuthContextProvider>
       <App />
+      <ToastContainer />
     </AuthContextProvider>
   </Router>,
 );
