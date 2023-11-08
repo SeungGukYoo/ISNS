7af7c68272c57cef4d0be8537d57b5c47870a7f2
SeungGukYoo <dbtmd878@gmail.com>
2023-11-08 23:39:52 +0900

    feat: 이메일 회원가입 기능 추가

--- src/util/firebaseClient.ts
+++ src/util/firebaseClient.ts
@@ -1,4 +1,4 @@
-import { getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import app from 'firebaseApp';
 
 class FirebaseClient {
@@ -6,6 +6,14 @@ class FirebaseClient {
 
   getAuthData() {
     return getAuth(app);
+  }
+  createEmailUser(email: string, password: string) {
+    const auth = getAuth(app);
+
+    return createUserWithEmailAndPassword(auth, email, password).then(userCredential => {
+      const user = userCredential.user;
+      return user;
+    });
   }
 }
 
