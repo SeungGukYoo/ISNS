82b45ec4317d933350cf660e41d1569139d001c2
SeungGukYoo <dbtmd878@gmail.com>
2023-11-08 14:32:56 +0900

    feat: 하단 네비게이션 구현

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import type { Props } from '../../';
+import Navigation from './Navigation';
 
 const Layout = ({ children }: Props) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <div className="layout">
+      {children}
+      <Navigation />
+    </div>
+  );
 };
 
 export default Layout;
