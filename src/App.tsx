914f086ca58a4fd1899682fc32951ba5258f08ee
SeungGukYoo <dbtmd878@gmail.com>
2023-11-08 13:49:17 +0900

    feat: 공통 레이아웃 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Layout from 'components/Layout';
 import { Outlet } from 'react-router-dom';
-import './App.css';
 
 function App() {
-  return <Outlet />;
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { Props } from '../../';
+
+const Layout = ({ children }: Props) => {
+  return <div className="layout">{children}</div>;
+};
+
+export default Layout;
