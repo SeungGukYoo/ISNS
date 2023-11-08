63057a4c0f4ab7fe1a755f4311e0870a6052bbf0
SeungGukYoo <dbtmd878@gmail.com>
2023-11-08 22:50:17 +0900

    feat: 인증 context 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
-import { router } from 'components/Router';
+import App from 'App';
+
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+
+import AuthContextProvider from 'context/authContext';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Router>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </Router>,
+);
