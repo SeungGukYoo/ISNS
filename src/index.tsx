c1a1715433a3c2f52443d147aea5322649ec97ef
SeungGukYoo <dbtmd878@gmail.com>
2023-11-08 13:49:47 +0900

    style: scss 파일 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { router } from 'components/Router';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
+import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<RouterProvider router={router} />);
