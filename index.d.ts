ff0bb744309cbc5aea9c6a1e4a736e5b6f069726
SeungGukYoo <dbtmd878@gmail.com>
2023-11-08 13:48:34 +0900

    feat: 전역 타입 추가

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+export interface Props {
+  children: React.ReactNode;
+}
+
+export interface PostProps {
+  id: string;
+  email: string;
+  content: string;
+  createdAt: string;
+  uid: string;
+  profileUrl?: string;
+  likes?: string[];
+  likeCount?: number;
+  comments?: string[];
+}
