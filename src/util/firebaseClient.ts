d2dc550a8cdc8ca0aa20a58ae2026fc8bc7378af
SeungGukYoo <dbtmd878@gmail.com>
2023-11-09 16:26:09 +0900

    feat: 구글, 깃헙 로그인 기능 추가

--- src/util/firebaseClient.ts
+++ src/util/firebaseClient.ts
@@ -1,10 +1,13 @@
 import {
   Auth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
   User,
   createUserWithEmailAndPassword,
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
 } from 'firebase/auth';
 import app from 'firebaseApp';
@@ -13,11 +16,38 @@ interface FirebaseClientType {
   getAuthData(): Auth;
   createEmailUser(email: string, password: string): Promise<User>;
   loginEmail(email: string, password: string): Promise<User>;
+  companyLogin(company: string): Promise<User | undefined>;
+  loginGoogle(): Promise<User>;
+  loginGithub(): Promise<User>;
   authChanged(callback: (user: User | null) => void): void;
   logoutUser(): Promise<void>;
 }
 
 class FirebaseClient implements FirebaseClientType {
+  async companyLogin(company: string): Promise<User | undefined> {
+    let userInfo: User | undefined;
+    if (company === 'google') userInfo = await this.loginGoogle();
+    else if (company === 'github') userInfo = await this.loginGithub();
+
+    if (userInfo) return userInfo;
+    throw new Error('일치하지 않음');
+  }
+  loginGoogle() {
+    const auth = this.getAuthData();
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider).then(result => {
+      const user = result.user;
+      return user;
+    });
+  }
+  loginGithub(): Promise<User> {
+    const auth = this.getAuthData();
+    const provider = new GithubAuthProvider();
+    return signInWithPopup(auth, provider).then(result => {
+      const user = result.user;
+      return user;
+    });
+  }
   logoutUser() {
     const auth = this.getAuthData();
     return signOut(auth);
