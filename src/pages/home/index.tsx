dd2bfff3672526f40a01290189446e5e41fe049b
SeungGukYoo <dbtmd878@gmail.com>
2023-11-09 17:06:49 +0900

    feat: 더미데이터 실제 데이터로 변경

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,77 +1,11 @@
 import PostBox from 'components/PostBox';
 import PostForm from 'components/PostForm';
 
-import type { PostProps } from '../../..';
-const posts: PostProps[] = [
-  {
-    id: '1',
-    email: 'test1@email.com',
-    content: 'content text',
-    createdAt: '2023-11-08 Tue',
-    uid: 'unique ID',
-    profileUrl: '',
-    likeCount: 1,
-    likes: ['a'],
-    comments: [],
-  },
-  {
-    id: '2',
-    email: 'test2@email.com',
-    content: 'content text2',
-    createdAt: '2023-11-08 Tue',
-    uid: 'unique ID2',
-    profileUrl: '',
-    likeCount: 3,
-    likes: ['a'],
-    comments: ['a'],
-  },
-  {
-    id: '3',
-    email: 'test3@email.com',
-    content: 'content tex3',
-    createdAt: '2023-11-08 Tue',
-    uid: 'unique ID3',
-    profileUrl: '',
-    likeCount: 3,
-    likes: ['a', 'b', 'c'],
-    comments: ['a', 'b'],
-  },
-  {
-    id: '4',
-    email: 'test1@email.com',
-    content: 'content text',
-    createdAt: '2023-11-08 Tue',
-    uid: 'unique ID',
-    profileUrl: '',
-    likeCount: 1,
-    likes: ['a'],
-    comments: [],
-  },
-  {
-    id: '5',
-    email: 'test2@email.com',
-    content: 'content text2',
-    createdAt: '2023-11-08 Tue',
-    uid: 'unique ID2',
-    profileUrl: '',
-    likeCount: 3,
-    likes: ['a'],
-    comments: ['a'],
-  },
-  {
-    id: '6',
-    email: 'test3@email.com',
-    content: 'content tex3',
-    createdAt: '2023-11-08 Tue',
-    uid: 'unique ID3',
-    profileUrl: '',
-    likeCount: 3,
-    likes: ['a', 'b', 'c'],
-    comments: ['a', 'b'],
-  },
-];
+import { PostsContext } from 'context/postsContext';
+import { useContext } from 'react';
 
 function HomePage() {
+  const { posts } = useContext(PostsContext);
   return (
     <div className="home">
       <div className="home__title">Home</div>
