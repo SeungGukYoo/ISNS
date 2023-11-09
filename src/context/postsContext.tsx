3a7a8d8142dd8f3b92acb562979c156277fd3f67
SeungGukYoo <dbtmd878@gmail.com>
2023-11-09 17:40:45 +0900

    feat: post context 구현

--- src/context/postsContext.tsx
+++ src/context/postsContext.tsx
@@ -1,29 +1,28 @@
-import { doc } from 'firebase/firestore';
-import { db } from 'firebaseApp';
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 import { PostProps, Props } from '../..';
 import { AuthContext } from './authContext';
 
 export const PostsContext = createContext({
   posts: [] as PostProps[],
+  getPosts: () => {},
 });
 
 const PostContextProvider = ({ children }: Props) => {
   const { firebaseClient, user } = useContext(AuthContext);
   const [posts, setPosts] = useState<PostProps[]>([]);
-  useEffect(() => {
-    const getPosts = async () => {
-      if (!user) return;
-      const postsArr: PostProps[] = [];
-      const querySnapShot = await firebaseClient?.getAllPosts();
-      querySnapShot?.forEach(doc => {
-        postsArr.push({ id: doc.id, ...doc.data() } as PostProps);
-      });
-      setPosts(postsArr);
-    };
-    getPosts();
+  const getPosts = useCallback(async () => {
+    if (!user) return;
+    const postsArr: PostProps[] = [];
+    const querySnapShot = await firebaseClient?.getAllPosts();
+    querySnapShot?.forEach(doc => {
+      postsArr.push({ id: doc.id, ...doc.data() } as PostProps);
+    });
+    setPosts(postsArr);
   }, [firebaseClient, user]);
-  return <PostsContext.Provider value={{ posts }}>{children}</PostsContext.Provider>;
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return <PostsContext.Provider value={{ posts, getPosts }}>{children}</PostsContext.Provider>;
 };
 
 export default PostContextProvider;
