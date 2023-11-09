e9c93ea144606ff18f8972714ad40619511fdfb2
SeungGukYoo <dbtmd878@gmail.com>
2023-11-09 15:36:48 +0900

    feat: 로그아웃 기능 구현

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,27 @@
+import { AuthContext } from 'context/authContext';
+import { useContext } from 'react';
 import { BiUser } from 'react-icons/bi';
 import { BsHouse } from 'react-icons/bs';
-import { IoIosLogOut } from 'react-icons/io';
+import { IoIosLogIn, IoIosLogOut } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function Navigation() {
   const navigate = useNavigate();
+  const { user, firebaseClient } = useContext(AuthContext);
+  const onLogout = async () => {
+    try {
+      if (firebaseClient) {
+        await toast.promise(firebaseClient.logoutUser(), {
+          pending: '잠시만 기다려주세요.',
+          success: '로그아웃이 되었습니다.',
+          error: '예기치 못한 에러가 발생했습니다.',
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <nav className="navigation">
       <div className="navigation__grid">
@@ -14,10 +31,17 @@ export default function Navigation() {
         <button type="button" onClick={() => navigate('/profile')}>
           <BiUser className="navigation__icon" /> Profile
         </button>
-        <button type="button" onClick={() => navigate('/')}>
-          <IoIosLogOut className="navigation__icon" />
-          Logout
-        </button>
+        {user ? (
+          <button type="button" onClick={onLogout}>
+            <IoIosLogOut className="navigation__icon" />
+            Logout
+          </button>
+        ) : (
+          <button type="button" onClick={() => navigate('/signin')}>
+            <IoIosLogIn className="navigation__icon" />
+            Login
+          </button>
+        )}
       </div>
     </nav>
   );
