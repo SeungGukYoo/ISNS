1bc4dacb3132c4a05c03f9ef8510781f12e0be53
SeungGukYoo <dbtmd878@gmail.com>
2023-11-09 14:35:51 +0900

    feat: 로그인 관찰자 이펙트 추가

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,4 +1,4 @@
-import { User } from 'firebase/auth';
+import { User, onAuthStateChanged } from 'firebase/auth';
 import { createContext, useEffect, useState } from 'react';
 import FirebaseClient from 'util/firebaseClient';
 import { Props } from '../..';
@@ -6,17 +6,26 @@ import { Props } from '../..';
 export const AuthContext = createContext({
   firebaseClient: null as FirebaseClient | null,
   user: null as User | null,
+  init: false,
 });
 
 const AuthContextProvider = ({ children }: Props) => {
   const [user, setUser] = useState<null | User>(null);
+  const [init, setInit] = useState(false);
   const firebaseClient = new FirebaseClient();
 
   useEffect(() => {
-    const auth = firebaseClient.getAuthData();
-    auth.currentUser && setUser(auth.currentUser);
-  }, [firebaseClient]);
-  return <AuthContext.Provider value={{ firebaseClient, user }}>{children}</AuthContext.Provider>;
+    firebaseClient.authChanged(setUser);
+    return setInit(true);
+  }, [firebaseClient, setInit, user]);
+
+  // useEffect(() => {
+  //   const auth = firebaseClient.getAuthData();
+  //   auth.currentUser && setUser(auth.currentUser);
+  // }, [firebaseClient]);
+  return (
+    <AuthContext.Provider value={{ firebaseClient, user, init }}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
