d3db2a32d5a1d426187f09bf6568e9cf5bee6cd8
SeungGukYoo <dbtmd878@gmail.com>
2023-11-09 14:35:02 +0900

    feat: 로그인, 관찰자 메서드 추가, 타입 수정

--- src/util/firebaseClient.ts
+++ src/util/firebaseClient.ts
@@ -1,14 +1,41 @@
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import {
+  Auth,
+  User,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import app from 'firebaseApp';
 
-class FirebaseClient {
-  constructor() {}
+interface FirebaseClientType {
+  getAuthData(): Auth;
+  createEmailUser(email: string, password: string): Promise<User>;
+  loginEmail(email: string, password: string): Promise<User>;
+  authChanged(callback: (user: User | null) => void): void;
+}
 
+class FirebaseClient implements FirebaseClientType {
+  authChanged(callback: (user: User | null) => void) {
+    const auth = this.getAuthData();
+    onAuthStateChanged(auth, user => {
+      if (user) callback(user);
+      else callback(null);
+    });
+  }
   getAuthData() {
     return getAuth(app);
   }
+  loginEmail(email: string, password: string) {
+    const auth = this.getAuthData();
+
+    return signInWithEmailAndPassword(auth, email, password).then(userCredential => {
+      const user = userCredential.user;
+      return user;
+    });
+  }
   createEmailUser(email: string, password: string) {
-    const auth = getAuth(app);
+    const auth = this.getAuthData();
 
     return createUserWithEmailAndPassword(auth, email, password).then(userCredential => {
       const user = userCredential.user;
