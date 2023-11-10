b9549b881ec51ee698a017d93cd4de49a1be5684
SeungGukYoo <dbtmd878@gmail.com>
2023-11-10 14:37:06 +0900

    feat: 콘텍스트 훅 변경 리액트 토스티파이 추가

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,12 +1,13 @@
-import { AuthContext } from 'context/authContext';
-import { PostsContext } from 'context/postsContext';
-import React, { useContext, useState } from 'react';
+import { DocumentData, DocumentReference } from 'firebase/firestore';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { PostProps } from '../..';
+import { useAuthContext, usePostContext } from './useContextUtil';
 
 const useForm = () => {
-  const { user, firebaseClient } = useContext(AuthContext);
-  const { getPosts } = useContext(PostsContext);
+  const { user, firebaseClient } = useAuthContext();
+  const { getPosts } = usePostContext();
   const [content, setContent] = useState('');
-  const [error, setError] = useState<null | string>(null);
 
   const onChangeValue = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value, name } = e.target;
@@ -16,28 +17,34 @@ const useForm = () => {
   };
   const onSubmitForm = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    if (content.trim().length < 5) {
-      setError('5글자 이상 입력해주시길 바랍니다.');
-      return;
-    }
+    if (!user) return;
 
     try {
-      const dataForm = {
+      const dataForm: Omit<PostProps, 'id'> = {
         content,
-        email: user?.email,
-        uid: user?.uid,
+        email: user.email || '',
+        uid: user.uid,
         createdAt: new Date().toLocaleDateString('ko', {
           hour: '2-digit',
           minute: '2-digit',
           second: '2-digit',
         }),
       };
-      const response = await firebaseClient?.addPost(dataForm);
-      if (response) {
-        setContent('');
-        getPosts();
-      }
+
+      await toast.promise(
+        firebaseClient?.addPost(dataForm) as Promise<DocumentReference<DocumentData, DocumentData>>,
+        {
+          pending: '잠시만 기다려주세요',
+          success: {
+            render() {
+              getPosts();
+              setContent('');
+              return '게시글을 작성하였습니다.';
+            },
+          },
+          error: '예기치못한 에러가 발생하였습니다.',
+        },
+      );
     } catch (error) {
       console.error(error);
     }
