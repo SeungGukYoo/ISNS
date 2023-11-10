13e80d9ca671cd89f9fcbfe4f271b9c591ef2696
SeungGukYoo <dbtmd878@gmail.com>
2023-11-10 17:21:00 +0900

    feat: onSnapShot함수로 변경

--- src/context/postsContext.tsx
+++ src/context/postsContext.tsx
@@ -1,30 +1,20 @@
-import { createContext, useCallback, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { PostProps, Props } from '../..';
 import { AuthContext } from './authContext';
 
 export const PostsContext = createContext({
   posts: [] as PostProps[],
-  getPosts: () => {},
 });
 
 const PostContextProvider = ({ children }: Props) => {
-  const { firebaseClient, user } = useContext(AuthContext);
+  const { firebaseClient } = useContext(AuthContext);
   const [posts, setPosts] = useState<PostProps[]>([]);
-  const getPosts = useCallback(async () => {
-    if (!user) return;
-    const postsArr: PostProps[] = [];
-    const sortedQuerySnapShot = await firebaseClient?.getSortedPosts();
 
-    sortedQuerySnapShot?.forEach(doc => {
-      postsArr.push({ id: doc.id, ...doc.data() } as PostProps);
-    });
-    setPosts(postsArr);
-  }, [firebaseClient, user]);
   useEffect(() => {
-    getPosts();
-  }, [getPosts]);
+    firebaseClient?.getPostsObserver(setPosts);
+  }, [setPosts, firebaseClient]);
 
-  return <PostsContext.Provider value={{ posts, getPosts }}>{children}</PostsContext.Provider>;
+  return <PostsContext.Provider value={{ posts }}>{children}</PostsContext.Provider>;
 };
 
 export default PostContextProvider;
