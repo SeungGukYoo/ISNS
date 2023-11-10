e7f111388e42cec388cc6653777e8f0bcb0f4017
SeungGukYoo <dbtmd878@gmail.com>
2023-11-10 14:12:50 +0900

    feat: 게시글이 없을 경우 컴포넌트 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,7 @@ import { useContext } from 'react';
 
 function HomePage() {
   const { posts } = useContext(PostsContext);
+
   return (
     <div className="home">
       <div className="home__title">Home</div>
@@ -15,9 +16,13 @@ function HomePage() {
       </div>
       <PostForm />
       <div className="post">
-        {posts.map(el => (
-          <PostBox post={el} key={el.id} />
-        ))}
+        {posts.length > 0 ? (
+          posts.map(el => <PostBox post={el} key={el.id} />)
+        ) : (
+          <div className="post__no-post">
+            <div className="post__text">게시글이 없습니다.</div>
+          </div>
+        )}
       </div>
     </div>
   );
