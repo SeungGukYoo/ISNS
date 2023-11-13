0dd7ffc0f4679dd7189bc16e0237f88ea72bb922
SeungGukYoo <dbtmd878@gmail.com>
2023-11-13 16:33:57 +0900

    feat: 검색 페이지 스타일링 및 구현

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,5 +1,36 @@
+import PostBox from 'components/post/PostBox';
+import useSearch from 'hooks/useSearch';
+
 function SearchPage() {
-  return <div>SearchPage</div>;
+  const { posts, query, onChangeValue } = useSearch();
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+        <div className="home__search-div">
+          <input
+            type="text"
+            className="home__search"
+            name="query"
+            onChange={onChangeValue}
+            value={query}
+            placeholder="# 없이 입력하세요 ex) #Daily → Daily"
+          />
+        </div>
+        <div className="post">
+          {posts.length > 0 ? (
+            posts.map(post => <PostBox post={post} key={post.id} />)
+          ) : (
+            <div className="post__no-post">
+              <div className="post__text">게시글이 없습니다.</div>
+            </div>
+          )}
+        </div>{' '}
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
