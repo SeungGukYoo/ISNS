36c6e95a1c376189cb2ff9e66f7bfb08a07e4b3e
SeungGukYoo <dbtmd878@gmail.com>
2023-11-14 17:52:02 +0900

    feat: 저장 함수 연결 및 스타일링

--- src/components/user/profilebox/ProfileBox.tsx
+++ src/components/user/profilebox/ProfileBox.tsx
@@ -3,18 +3,35 @@ import useProfile from 'hooks/useProfile';
 import { FaUserCircle } from 'react-icons/fa';
 import styles from './ProfileBox.module.scss';
 const ProfileBox = () => {
-  const { user, posts } = useProfile();
+  const { user, posts, isSave, profileUrl, updateProfile, uploadProfile } = useProfile();
 
   return (
     <div className={styles.profile}>
       <div className={styles.profile__container}>
         <div className={styles.profile__img}>
-          {user?.photoURL ? (
-            <img src={user?.photoURL} alt="user Profile" />
+          {profileUrl ? (
+            <img src={profileUrl} alt="user Profile" />
           ) : (
             <FaUserCircle className={styles.default__image} />
           )}
+          <label htmlFor="profile-file" className={styles.upload__btn}>
+            Upload Profile
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            id="profile-file"
+            name="profile-file"
+            className="hidden"
+            onChange={uploadProfile}
+          />
+          {isSave && (
+            <button onClick={updateProfile} className={styles.updateBtn}>
+              save
+            </button>
+          )}
         </div>
+
         <div>
           <div className={styles.profile__info}>
             <p className={styles.email}>Email: {user?.email}</p>
