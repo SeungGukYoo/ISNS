a162348dd32208bfd960ee1a5ffba5228c70b5b2
SeungGukYoo <dbtmd878@gmail.com>
2023-11-14 16:06:30 +0900

    feat: 포스트 레이아웃 및 스타일링

--- src/components/user/profilebox/ProfileBox.tsx
+++ src/components/user/profilebox/ProfileBox.tsx
@@ -1,15 +1,16 @@
+import PostBox from 'components/post/PostBox';
 import useProfile from 'hooks/useProfile';
 import { FaUserCircle } from 'react-icons/fa';
 import styles from './ProfileBox.module.scss';
 const ProfileBox = () => {
-  const { user } = useProfile();
+  const { user, posts } = useProfile();
 
   return (
     <div className={styles.profile}>
       <div className={styles.profile__container}>
         <div className={styles.profile__img}>
           {user?.photoURL ? (
-            <img src={user?.photoURL} alt="" />
+            <img src={user?.photoURL} alt="user Profile" />
           ) : (
             <FaUserCircle className={styles.default__image} />
           )}
@@ -18,6 +19,7 @@ const ProfileBox = () => {
           <div className={styles.profile__info}>
             <p className={styles.email}>Email: {user?.email}</p>
             <p className={styles.name}>Name: {user?.displayName}</p>
+            <p>Post Count: {posts.length}</p>
           </div>
           <div className={styles.profile__follower}>
             <p className={styles.follower}>Follower: 5</p>
@@ -26,6 +28,11 @@ const ProfileBox = () => {
         </div>
       </div>
       {/* post */}
+      <div className={styles.profile__posts}>
+        {posts.map(post => (
+          <PostBox post={post} key={post.id} />
+        ))}
+      </div>
     </div>
   );
 };
