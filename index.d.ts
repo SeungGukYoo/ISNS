c1201a5f9862ae30b433767031b75b658c4ca45a
SeungGukYoo <dbtmd878@gmail.com>
2023-11-20 16:43:16 +0900

    feat: 클래스 타입 분리

--- index.d.ts
+++ index.d.ts
@@ -24,3 +24,53 @@ export interface PostProps {
   comments?: CommentProps[];
   hashtags: string[];
 }
+
+export interface FirebaseClientType {
+  gitHubProvider: GithubAuthProvider;
+  googleProvider: GoogleAuthProvider;
+  // authenticate
+  getAuthData(): Auth;
+  authChanged(callback: React.Dispatch<React.SetStateAction<User | null>>): void;
+  createEmailUser(email: string, password: string): Promise<User>;
+  loginEmail(email: string, password: string): Promise<User>;
+  companyLogin(company: string): Promise<User | undefined>;
+  loginGoogle(): Promise<User>;
+  loginGithub(): Promise<User>;
+  logoutUser(): Promise<void>;
+  updateProfileData(downloadUrl: string, displayName: string): Promise<void>;
+
+  // store
+  postObserver(userUid: string | null, path: string): void;
+  getDocData(): DocumentReference<DocumentData, DocumentData>;
+  followObserver(
+    callBack: React.Dispatch<React.SetStateAction<boolean>>,
+    userId: string,
+    postId: string,
+  ): void;
+  getPostsObserver(callBack: React.Dispatch<React.SetStateAction<PostProps[]>>): void;
+  getPostObserver(
+    postId: string,
+    callBack: React.Dispatch<React.SetStateAction<PostProps | null>>,
+  ): void;
+  getFollower(callBack: React.Dispatch<React.SetStateAction<number>>, userId: string): void;
+  getFollowing(callBack: React.Dispatch<React.SetStateAction<number>>, userId: string): void;
+  getPost(pstId: string): Promise<DocumentSnapshot<DocumentData, DocumentData>>;
+  addPost(data: Omit<PostProps, 'id'>): Promise<DocumentReference<DocumentData, DocumentData>>;
+  updatePost(postId: string, postData: Omit<PostProps, 'id'>): Promise<unknown>;
+  deletePost(postId: string): Promise<void>;
+  searchPost(hashtag: string, callback: React.Dispatch<React.SetStateAction<PostProps[]>>): void;
+  getPersonalPost(uid: string): Promise<QuerySnapshot<DocumentData, DocumentData>>;
+  getLikePosts(uid: string): Promise<QuerySnapshot<DocumentData, DocumentData>>;
+  likePost(postId: string, userUid: string, likesCount: number): Promise<void>;
+  unLikePost(postId: string, userUid: string, likesCount: number): Promise<void>;
+  addComment(commentInfo: CommentProps, postId: string): Promise<void>;
+  deleteComment(userUid: CommentProps, postId: string): Promise<unknown>;
+  followingUser(myId: string, postId: string): Promise<void>;
+  followerUser(myId: string, postId: string): Promise<void>;
+  unfollowingUser(myId: string, postId: string): Promise<void>;
+  unfollowerUser(myId: string, postId: string): Promise<void>;
+  // storage
+  uploadImage(uuid: string, result: string): Promise<UploadResult>;
+  downloadImge(snapshot: UploadResult): Promise<string>;
+  deleteImage(uuid: string, imgUrl: string): Promise<void>;
+}
