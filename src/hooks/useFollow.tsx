92223ba6eaa7b2bb93d282c2af3f9be35bac7f82
SeungGukYoo <dbtmd878@gmail.com>
2023-11-21 15:34:54 +0900

    feat: 타인일 경우에만 알림을 저장을 위한 예외 처리

--- src/hooks/useFollow.tsx
+++ src/hooks/useFollow.tsx
@@ -16,20 +16,22 @@ const useFollow = () => {
   const onFollowing = async (postId: string) => {
     if (!user?.uid) return;
     try {
-      const notificationInfo = {
-        uid: user?.uid,
-        email: user?.email || '',
-        photoUrl: user?.photoURL,
-        createdAt: new Date().toLocaleDateString('ko', {
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-        }),
-        content: `${user?.email}님께서 팔로우하였습니다.`,
-        postId,
-        read: false,
-      };
-      await firebaseClient?.addNotification(notificationInfo);
+      if (user.uid !== postId) {
+        const notificationInfo = {
+          uid: user?.uid,
+          email: user?.email || '',
+          photoUrl: user?.photoURL,
+          createdAt: new Date().toLocaleDateString('ko', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+          }),
+          content: `${user?.email}님께서 팔로우하였습니다.`,
+          postId,
+          read: false,
+        };
+        await firebaseClient?.addNotification(notificationInfo);
+      }
       await firebaseClient?.followingUser(user?.uid, postId);
       await firebaseClient?.followerUser(user?.uid, postId);
     } catch (error) {
